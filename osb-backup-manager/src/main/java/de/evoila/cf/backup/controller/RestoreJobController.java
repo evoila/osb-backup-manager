83db6ad6d93f0c2580e1057cf0235c8e8e4ab1ca
Patrick Weber <pweber@evoila.de>
2021-09-19 23:58:21 +0200

    fix cleanup files

--- osb-backup-manager/src/main/java/de/evoila/cf/backup/controller/RestoreJobController.java
+++ osb-backup-manager/src/main/java/de/evoila/cf/backup/controller/RestoreJobController.java
@@ -15,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author Yannic Remmet, Johannes Hiemer.
