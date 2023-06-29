6f0bb97df506385e4354711aff41cae6060590ee
Marius Berger <mberger@evoila.de>
2023-06-29 18:06:51 +0200

    chore(BackupController): Add print of auth header for backup.

--- osb-backup-manager/src/main/java/de/evoila/cf/backup/controller/BackupController.java
+++ osb-backup-manager/src/main/java/de/evoila/cf/backup/controller/BackupController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -47,8 +48,9 @@ public class BackupController {
     @ApiOperation(value = "Add a new backup request to the queue. The backup will be executed asynchronously at the " +
             "scheduled interval as a job.")
     @RequestMapping(value = "/backup", method = RequestMethod.POST)
-    public ResponseEntity backup(@RequestBody BackupRequest backupRequest) {
+    public ResponseEntity backup(@RequestBody BackupRequest backupRequest, @RequestHeader String authorization) {
 
+        log.info("Authorization header: " + authorization);
         String instanceID = backupRequest.getBackupPlan().getServiceInstance().getId();
         if (!permissionCheckService.hasReadAccess(instanceID)) {
             throw new AuthenticationServiceException("User is not authorised to access the requested resource. Please contact your System Administrator.");
