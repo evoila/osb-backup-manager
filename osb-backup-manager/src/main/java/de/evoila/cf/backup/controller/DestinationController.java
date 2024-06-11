b75889b6e11bb73674534bd04d3c76dd3e6d5d32
MaxF <maxfuhrich@gmail.com>
2024-06-11 14:06:00 +0200

    feat: additional logs for a failed validation, additional information in ResponseEntity on error

--- osb-backup-manager/src/main/java/de/evoila/cf/backup/controller/DestinationController.java
+++ osb-backup-manager/src/main/java/de/evoila/cf/backup/controller/DestinationController.java
@@ -126,7 +126,8 @@ public class DestinationController extends BaseController {
             }
             return new ResponseEntity<>(destination, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>("Could not validate Endpoint. " + e.getMessage(), HttpStatus.BAD_REQUEST);
+            log.info("ERROR: Could not validate Endpoint. " + e.getMessage() + " Exception type: " + e.getClass());
+            return new ResponseEntity<>("Could not validate Endpoint. " + e.getMessage() + " Exception type: " + e.getClass(), HttpStatus.BAD_REQUEST);
         }
 
     }
